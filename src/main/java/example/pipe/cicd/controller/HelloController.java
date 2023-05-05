8518572a0a084e737e184da09cfcf2674c2cd5b0
서민재 <seominjae@seominjaeui-Macmini.local>
2023-05-05 21:24:34 +0900

    Test

--- src/main/java/example/pipe/cicd/controller/HelloController.java
+++ src/main/java/example/pipe/cicd/controller/HelloController.java
@@ -1,10 +1,14 @@
 package example.pipe.cicd.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${server.port}")
+    private Integer port;
 
     @GetMapping("/hello")
     public String hello() {
@@ -15,4 +19,19 @@ public class HelloController {
     public String home() {
         return "Home";
     }
+
+    @GetMapping("/test")
+    public String port() {
+        StringBuilder sb = new StringBuilder();
+        String container = port == 8081 ? "blue" : "green";
+
+
+       return sb.append("현재 실행중인 port 번호는 ")
+               .append(port)
+               .append("입니다. 현재 실행중인 container의 이름은 ")
+               .append(container)
+               .append("입니다.")
+               .toString();
+    }
+
 }
